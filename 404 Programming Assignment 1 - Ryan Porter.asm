92c9e59c063430530966c2ef39539549fa0e3040
Ryan Porter <porter.ryan42@gmail.com>
2022-07-31 23:25:50 -0400

    Update 404 Programming Assignment 1 - Ryan Porter.asm
    Fixed Formatting

--- 404 Programming Assignment 1 - Ryan Porter.asm
+++ 404 Programming Assignment 1 - Ryan Porter.asm
@@ -14,7 +14,7 @@ list: .word 7, 9 , 4, 3, 8, 1, 6, 2, 5
 .globl main
 
 main:
-	li $s0, 9 							# s0 = length
+	li $s0, 9 								# s0 = length
 	
 	li $v0, 4
 	la $a0, prompt1
@@ -26,18 +26,18 @@ main:
 
 	li $v0, 5
 	syscall 
-	move $s1,$v0 						# s1 = direction
+	move $s1,$v0 							# s1 = direction
 	
 	li $v0, 4
 	la $a0, beforesort
 	syscall
 	
 	la $a1, list
-	jal printData						# call printData function
+	jal printData							# call printData function
 	
 	
-	li $t0, 0							# t0 = k = 0
-	addi $t1, $s0, -1					# t1 = length-1
+	li $t0, 0								# t0 = k = 0
+	addi $t1, $s0, -1						# t1 = length-1
 	
 	la $a1, list
 	loop1:
@@ -64,19 +64,19 @@ main:
 		
 		
 		#sll $t3, $t2, 2					#t3 = min multiplied by 4
-		#lw $t4, list($t3)				# t4 = list[min]
+		#lw $t4, list($t3)					# t4 = list[min]
 		
 		#sll $t5, $t0, 2					# t5 = k multiplied by 4
-		#lw $t6, list($t5)				# t6 = list[k]
+		#lw $t6, list($t5)					# t6 = list[k]
 		
-		#sw $t6, list($3)				# list[min] = (old)list[k]
-		#sw $t4, list($5)				# list[k] = (old)list[min]
-		
-		
+		#sw $t6, list($3)					# list[min] = (old)list[k]
+		#sw $t4, list($5)					# list[k] = (old)list[min]
 		
 		
 		
-		next:	
+		
+		
+	next:	
 	addi $t0, $t0, 1
 	j loop1
 	loop1Exit:
@@ -87,21 +87,21 @@ main:
 
 
 printData:
-	bge $t0, $s0, exitPrintData			# if iterator >= size then exit
+	bge $t0, $s0, exitPrintData				# if iterator >= size then exit
 	
 	lw	$t1, 0($a1)						
 	
-	addi $a1, $a1, 4					# increment array
+	addi $a1, $a1, 4						# increment array
 	
 	li $v0, 1						
 	move $a0, $t1
 	syscall
 	
-	li $v0, 4							# print space
+	li $v0, 4								# print space
 	la $a0, space
 	syscall
 	
-	addi $t0, $t0, 1					# increment loop by 1
+	addi $t0, $t0, 1						# increment loop by 1
 	j printData
 	
 exitPrintData:
